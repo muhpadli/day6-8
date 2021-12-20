2b15af72ed4aa14fea36f590b7bdd1ac22896587
muhpadli <96329788+muhpadli@users.noreply.github.com>
2021-12-20 17:26:29 +0800

    Update Day 8.py

--- Day 8.py
+++ Day 8.py
@@ -1,6 +1,6 @@
 buah = ["mangga", "pisang", "melon", "jambu"]
 print(buah)
-
+#menampilkan data list
 print(buah[0])
 print(buah[1])
 print(buah[2])
@@ -18,3 +18,17 @@ print()
 buah.insert(3, "pepaya")
 print(buah)
 print()
+#menghapus dua data list
+buah.remove("pisang")
+print(buah)
+del buah[1:3]
+print(buah)
+print()
+#menggabungkan dua nilai data list
+buah = [ "mangga", "pisang", "melon", "jambu"]
+buah1 = [ "semangka", "anggur", "apel"]
+buah2 = buah + buah1
+print(buah2)
+#mengurutkan data list
+buah2.sort()
+print(buah)
